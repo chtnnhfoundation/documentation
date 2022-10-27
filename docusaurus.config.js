83057b9a640d6f1a66eb677092e8b2a8c6ad4fe0
Chaitanya Mittal <chtnnh@gmail.com>
2022-10-27 22:28:01 +0400

    fix: add trailing slash to baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://chtnnhfoundation.github.io',
-  baseUrl: '/documentation',
+  baseUrl: '/documentation/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
